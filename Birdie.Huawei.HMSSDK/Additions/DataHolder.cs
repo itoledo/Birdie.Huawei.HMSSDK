a89789491cc7cdf355f0cb4f556b4fbddf10a81c
Ivan Toledo <itoledo@birdie.cl>
2020-03-15 00:37:14 -0300

    Add project files.

---
+++ Birdie.Huawei.HMSSDK/Additions/DataHolder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Com.Huawei.Hms.Common.Data
+{
+    public partial class DataHolder
+    {
+        ~DataHolder()
+        {
+            Finalizer();
+        }
+    }
+
+    public partial class DBInnerIter
+    {
+        public void Remove() { }
+    }
+}
+
